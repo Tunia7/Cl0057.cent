f1278da94c43ecf0b23f294a1317fe273374f1ce
Tunia <saint.wolfer@gmail.com>
2021-11-16 14:21:48 +0100

    poprawki

--- get_pdb_all_proteins_in_CL0057.py
+++ get_pdb_all_proteins_in_CL0057.py
@@ -1,4 +1,6 @@
 import wget
+
+
 def otwieranie_pliku(nazwa_pliku):
     plik = open(nazwa_pliku)
     linijki = plik.readlines()
@@ -6,15 +8,20 @@ def otwieranie_pliku(nazwa_pliku):
     linijki = [linia.strip().split() for linia in linijki]
     return linijki
 
+
 def pobieranie_pdb(plik):
-    pliki=[]
-    białka=otwieranie_pliku(plik)[1:]
+    pliki = []
+    białka = otwieranie_pliku(plik)[1:]
     for białko in białka:
-        if len(białko)==3:
-            id_pdb=białko[2].split(";")
+        if len(białko) == 3:
+            id_pdb = białko[2].split(";")
             for id in id_pdb[:-1]:
-                wget.download(f"https://files.rcsb.org/download/{id}.pdb",f"{białko[0]}_{białko[1]}_{id}.pdb")
-    pliki.append(f"{białko[0]}_{białko[1]}_{id}.pdb")
+                try:
+                    wget.download(f"https://files.rcsb.org/download/{id}.pdb", f"{białko[0]}_{białko[1]}_{id}.pdb")
+                    pliki.append(f"{białko[0]}_{białko[1]}_{id}.pdb")
+                except:
+                    pass
+    return pliki
 
 
 print(pobieranie_pdb("cl0057_with_pdb_references.txt"))
