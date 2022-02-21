03faf12c81c456a0a12d981c2828e41a179e1be5
Tunia <saint.wolfer@gmail.com>
2022-02-21 12:51:50 +0100

    fasta z uniprota

--- pfam_alphafold_get_pdb.py
+++ pfam_alphafold_get_pdb.py
@@ -1,4 +1,5 @@
 import wget
+import os
 
 
 # pobiera listę uniprot id i organizm id  białek należących do klanu
@@ -19,28 +20,33 @@ alphafold_organizmy = ['7955', '237562', '10090', '83333', '7227', '243232', '36
                        '353153', '559292', '284812', '10116', '9606', '3702', '4577', '5671', '93061', '3847', '39947']
 
 nasze_bialka = []
-plik = open("baza.txt").readlines()
-for wynik in plik:
-    wynik = wynik.split()
-    id = wynik[0]
-    organizm = wynik[1]
-    if organizm in alphafold_organizmy:
-        nasze_bialka.append(wynik)
+if os.path.isfile("baza.txt")==0:
+    rozbudowa_linku()
+with open("baza.txt") as plik:
+    for wynik in plik.readlines():
+        wynik = wynik.split()
+        id = wynik[0]
+        organizm = wynik[1]
+        if organizm in alphafold_organizmy:
+            nasze_bialka.append(wynik)
 
-rozbudowa_linku("cl0057")
 # plik 'cl0057' zawiera listę rodzin z klanu w jednej linijce rodzielone spacją
 
 
-licznik = 0
-wynik = []
-
+wynik = ""
 # tworzy tablicę z nazwami plików pdb, które uda nam sie pobrać
 for protein in nasze_bialka:
-    try:
-        wget.download(rf'https://alphafold.ebi.ac.uk/files/AF-{protein[0]}-F1-model_v1.pdb',
-                      f"{protein[0]}_{protein[1]}.pdb")
-        wynik.append(f'{protein[0]}_{protein[1]}.pdb')
-    except:
-        pass
-
+    if 0 == os.path.isfile(f'{protein[0]}_{protein[1]}.pdb'):
+        try:
+            wget.download(rf'https://alphafold.ebi.ac.uk/files/AF-{protein[0]}-F1-model_v1.pdb',
+                          f"{protein[0]}_{protein[1]}.pdb")
+        except:
+            pass
+    if 1 == os.path.isfile(f'{protein[0]}_{protein[1]}.pdb'):
+        wget.download(rf'https://www.uniprot.org/uniprot/{protein[0]}.fasta', f'{protein[0]}_{protein[1]}.fasta')
+        with open(f'{protein[0]}_{protein[1]}.fasta') as plik:
+            wynik += f'>alphafold_{protein[0]}_{protein[1]}\n'
+            for linia in plik.readlines()[1:]:
+                wynik += linia.strip()
+            wynik += "\n"
 print(wynik)
