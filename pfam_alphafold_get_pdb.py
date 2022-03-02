b5356c4c8594d590dd803b219bfab927f4e4a44a
Tunia7 <saint.wolfer@gmail.com>
2022-03-02 10:30:52 +0100

    bub

--- pfam_alphafold_get_pdb.py
+++ pfam_alphafold_get_pdb.py
@@ -20,7 +20,7 @@ alphafold_organizmy = ['7955', '237562', '10090', '83333', '7227', '243232', '36
                        '353153', '559292', '284812', '10116', '9606', '3702', '4577', '5671', '93061', '3847', '39947']
 
 nasze_bialka = []
-if os.path.isfile("baza.txt")==0:
+if os.path.isfile("baza.txt") == 0:
     rozbudowa_linku()
 with open("baza.txt") as plik:
     for wynik in plik.readlines():
@@ -32,6 +32,14 @@ with open("baza.txt") as plik:
 
 # plik 'cl0057' zawiera listę rodzin z klanu w jednej linijce rodzielone spacją
 
+
+# zakresy
+słownik_zakresow = {}
+with open(r"zakresy.txt") as zakresy:
+    for linia in zakresy.readlines():
+        print(linia)
+        linia = linia.strip().split("/")
+        słownik_zakresow[linia[0][1:]] = linia[1].split("-")
 
 wynik = ""
 # tworzy tablicę z nazwami plików pdb, które uda nam sie pobrać
@@ -45,8 +53,12 @@ for protein in nasze_bialka:
     if 1 == os.path.isfile(f'{protein[0]}_{protein[1]}.pdb'):
         wget.download(rf'https://www.uniprot.org/uniprot/{protein[0]}.fasta', f'{protein[0]}_{protein[1]}.fasta')
         with open(f'{protein[0]}_{protein[1]}.fasta') as plik:
-            wynik += f'>alphafold_{protein[0]}_{protein[1]}\n'
-            for linia in plik.readlines()[1:]:
-                wynik += linia.strip()
-            wynik += "\n"
+            if protein[0] in słownik_zakresow.keys():
+                wynik += f'>alphafold_{protein[0]}_{protein[1]}|\n'
+
+                zakres_odpowiedni=słownik_zakresow[protein[0]]
+                pom=""
+                for linia in plik.readlines()[1:]:
+                    pom += linia.strip()
+                wynik +=pom[int(zakres_odpowiedni[0]):int(zakres_odpowiedni[1])] + "\n"
 print(wynik)
--- zakresy_rhh_z_pfam.py
+++ zakresy_rhh_z_pfam.py
@@ -10,16 +10,15 @@ for linia in html_kod:
         rodziny.append(linia[14:-2])  # przykładowa linijka to "href="/family/PF19807">"
 wszystkie_zakresy = ""
 for rodzina in rodziny:
-    if 0 == os.path.isfile(f"{rodzina}.txt"):
+    if 0 == os.path.isfile(f"{rodzina}.fasta"):
         wget.download(
-            fr"http://pfam.xfam.org/family/{rodzina}/alignment/full/format?format=pfam&alnType=full&order=t&case=l&gaps=dashes&download=0",
-            f"{rodzina}.txt")
-    with open(f"{rodzina}.txt") as plik:
+            fr"http://pfam.xfam.org/family/{rodzina}/alignment/full/format?format=fasta&alnType=full&order=t&case=l&gaps=default&download=0",
+            f"{rodzina}.fasta")
+    with open(f"{rodzina}.fasta") as plik:
         dane = plik.readlines()
         for linia in dane:
             wszystkie_zakresy += linia
 
+
+
 print(wszystkie_zakresy)
-plik = open("wszystkie_zakresy_z_pfam.txt", "w")
-plik.write(wszystkie_zakresy)
-plik.close()
